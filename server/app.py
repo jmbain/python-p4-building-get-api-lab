17a111cbb9dc489c91c2f90b70d7714deb855290
Josh Bain <joshuambain@gmail.com>
2024-09-18 14:24:04 -0400

    completed building get api lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,48 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    
+    response = make_response(
+        bakeries,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response = make_response(
+        bakery_dict,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    #Still need to get this to return descending by price
+    baked_goods = [baked_good.to_dict() for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    response = make_response(
+        baked_goods,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    #Still need to get this to return descending by price AND limit to 1 item
+    baked_goods = [baked_good.to_dict() for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    response = make_response(
+        baked_goods[0],
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
